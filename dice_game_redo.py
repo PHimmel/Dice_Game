3edebc678d202678f85e2142b70ce7cf6177f1b4
PHimmel <peterhimmelman@yahoo.com>
2020-12-08 19:37:01 -0500

    expanding on revision

--- dice_game_redo.py
+++ dice_game_redo.py
@@ -4,23 +4,42 @@ A dice game.
 """
 
 from random import randint
+from time import sleep
 
 
-def get_number_of_roles():
+def get_number_of_rolls():
     return int(raw_input("How many roll would you like to play?\n"))
 
 
 def roll_the_die():
-    return randint(0, 6)
+    return randint(0, 5) + 1
 
-class Game:
 
-    def __init__(self, rolls, winner=0):
-        self.rolls = rolls
-        self.winner = winner
+def dice_turn(user):
+    turn = roll_the_die()
+    print('{} rolled a {}'.format(user, turn))
+    return turn
+
+# class Game:
+#
+#     def __init__(self, rolls, winner=0):
+#         self.rolls = rolls
+#         self.winner = winner
 
 
 def main():
-    rolls = get_number_of_roles()
+    rolls = get_number_of_rolls() * 2
 
-    play = Game(rolls,)
+    while rolls > 0:
+        if rolls % 2 == 0:
+            user = 'You'
+        else:
+            user = 'Computer'
+        number = dice_turn(user)
+
+        sleep(1)
+        rolls -= 1
+
+
+if __name__ == '__main__':
+    main()
