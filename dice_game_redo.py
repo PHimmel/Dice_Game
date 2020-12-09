13b3964dfad4efa3100180daabcba65ab75c49c5
PHimmel <peterhimmelman@yahoo.com>
2020-12-08 23:15:32 -0500

    added to it, must change from version 2.7 to > 3.5 python

--- dice_game_redo.py
+++ dice_game_redo.py
@@ -6,9 +6,12 @@ A dice game.
 from random import randint
 from time import sleep
 
+# list for counting wins/losses/ties
+human_win = []
+
 
 def get_number_of_rolls():
-    return int(raw_input("How many roll would you like to play?\n"))
+    return int(raw_input("How many rolls would you like to play?\n"))
 
 
 def roll_the_die():
@@ -26,19 +29,38 @@ def dice_turn(user):
 #         self.rolls = rolls
 #         self.winner = winner
 
+# main logic for determining winner
+def compare_list_positions(ls):
+
+    if ls[0] > ls[1]:
+        print('You win!')
+        human_win.append(1)
+    elif ls[1] > ls[0]:
+        print('Computer win!')
+        human_win.append(0)
+    else:
+        print('Tie!')
+        human_win.append(None)
+
 
 def main():
     rolls = get_number_of_rolls() * 2
+    turns = []
 
     while rolls > 0:
         if rolls % 2 == 0:
-            user = 'You'
+            turn = dice_turn('You')
+            turns.append(turn)
         else:
-            user = 'Computer'
-        number = dice_turn(user)
+            turn = dice_turn('computer')
+            turns.append(turn)
+            compare_list_positions(turns)
+            turns = []
 
         sleep(1)
         rolls -= 1
+
+    print(human_win)
 
 
 if __name__ == '__main__':
